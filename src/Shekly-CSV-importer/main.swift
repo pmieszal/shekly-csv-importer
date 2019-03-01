0cd033d01979196dd01f600327ebe6d80b8449a9
Patryk Mieszała <patryk.mieszala@gmail.com>
2019-03-01 17:41:43 +0100

    Initial Commit

---
+++ src/Shekly-CSV-importer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Shekly-CSV-importer
+//
+//  Created by Patryk Mieszała on 01/03/2019.
+//  Copyright © 2019 Patryk Mieszała. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
